96fb2fbe37ab79c910163e12a034eec74f900b87
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-10 15:25:39 +0900

    added dummy list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const DUMMY_LIST = [
+    { id: 1, title: "Task 1", text: "Some gibberish" },
+    { id: 2, title: "Task 2", text: "Some gibberish" },
+    { id: 3, title: "Task 3", text: "Some gibberish" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>Title</h1>
+      <div>
+        <h2>List</h2>
+        <ul>
+          {DUMMY_LIST.map((item) => (
+            <li>
+              <h1>{item.title}</h1>
+              <hp>{item.text}</hp>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
