55498b0faa128aa27845a0cbfb296eaa5562bd89
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-10 16:02:21 +0900

    submit handler working

--- src/Components/Todo/UserInput/Input.js
+++ src/Components/Todo/UserInput/Input.js
@@ -8,8 +8,14 @@ const Input = (props) => {
     setTitle(event.target.value);
   };
 
+  const setDescriptionHandler = (event) => {
+    setDescription(event.target.value);
+  };
+
   const formSubmitHandler = (event) => {
-    event.preventeDefault();
+    event.preventDefault();
+
+    console.log(`New Task Added\n ${title}: ${description}`);
   };
 
   return (
@@ -23,7 +29,12 @@ const Input = (props) => {
           onChange={setTitleHandler}
         />
         <label htmlFor="description">Description</label>
-        <input id="description" type="text" value="{description}" onChange="" />
+        <input
+          id="description"
+          type="text"
+          value={description}
+          onChange={setDescriptionHandler}
+        />
         <button type="submit" onClick={formSubmitHandler}>
           {" "}
           Add a task
