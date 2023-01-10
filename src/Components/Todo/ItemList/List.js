5ddda3ede6e334feb5300a4e6d3ca13b7bd9e23b
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-10 16:46:47 +0900

    bugfix

--- src/Components/Todo/ItemList/List.js
+++ src/Components/Todo/ItemList/List.js
@@ -4,7 +4,7 @@ const List = (props) => {
       {props.items.map((item) => (
         <li key={item.id}>
           <h1>{item.title}</h1>
-          <p>{item.descriptionga .}</p>
+          <p>{item.descriptionga}</p>
         </li>
       ))}
     </ul>
