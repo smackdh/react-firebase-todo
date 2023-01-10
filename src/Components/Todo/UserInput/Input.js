d9406ec6297d22d1ecbffeb80109720d47892f2b
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-10 15:52:18 +0900

    added input form

--- src/Components/Todo/UserInput/Input.js
+++ src/Components/Todo/UserInput/Input.js
@@ -1,5 +1,14 @@
 const Input = () => {
-  return console.log("Input test");
+  return (
+    <div>
+      <form>
+        <label htmlFor="title">Title</label>
+        <input id="title" type="text" value="{title}" onChange="" />
+        <label htmlFor="description">Description</label>
+        <input id="description" type="text" value="{description}" onChange="" />
+      </form>
+    </div>
+  );
 };
 
 export default Input;
