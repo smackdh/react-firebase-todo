5290250a3248a1d808896a0914c2a67aa0da1033
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-13 09:16:00 +0900

    added editview

--- src/Components/Todo/Item/Item.js
+++ src/Components/Todo/Item/Item.js
@@ -1,20 +1,38 @@
+import React, { useState } from "react";
+
 const Item = (props) => {
-  return (
-    <div>
-      <li>
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
-        <input
-          type="checkbox"
-          checked={props.completed}
-          onChange={() => props.toggleCompleted(props.id)}
-        />
-        <button type="button" onClick={props.removeItem}>
-          Delete
-        </button>
-      </li>
-    </div>
+  const [isEditing, setisEditing] = useState(false);
+
+  const viewTemplate = (
+    <li>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+      <input
+        type="checkbox"
+        checked={props.completed}
+        onChange={() => props.toggleCompleted(props.id)}
+      />
+
+      <button type="button" onClick={() => setisEditing(true)}>
+        Edit
+      </button>
+    </li>
   );
+
+  const editTemplate = (
+    <li>
+      <h1>Editing</h1>
+      <p>Editing</p>
+      <button type="button" onClick={props.removeItem}>
+        Delete
+      </button>
+      <button type="button" onClick={() => setisEditing(false)}>
+        Cancel
+      </button>
+    </li>
+  );
+
+  return <div>{isEditing ? editTemplate : viewTemplate}</div>;
 };
 
 export default Item;
